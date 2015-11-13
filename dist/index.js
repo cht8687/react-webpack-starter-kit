3475143b9d9f601df22b4bdbb42be92df2eb11f6
Robert Chang <cht8687@gmail.com>
2015-11-14 08:51:07 +1100

    remove junk

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _App = require('./App');
+
+var _App2 = _interopRequireDefault(_App);
+
+_react2['default'].render(_react2['default'].createElement(_App2['default'], null), document.body);
+//# sourceMappingURL=index.js.map
